10f11facec47140ccb1bd068b6e358dd18730e99
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-08-25 02:13:29 +0800

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -157,7 +157,7 @@ def start(update: Update, context: CallbackContext):
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([[
                     InlineKeyboardButton(
-                        text="Add Saitama to your group",
+                        text="Add TakeshiBot to your Group",
                         url="t.me/{}?startgroup=true".format(
                             context.bot.username))
                 ]]))
