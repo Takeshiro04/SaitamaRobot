c37daa955ed9ec4297964ebbd5cdf292bcebef65
Takeshiro04 <61047914+Takeshiro04@users.noreply.github.com>
2020-08-25 02:59:56 +0800

    Update global_bans.py

--- SaitamaRobot/modules/global_bans.py
+++ SaitamaRobot/modules/global_bans.py
@@ -495,7 +495,7 @@ you and your groups by removing spam flooders as quickly as possible. They can b
 `/gbanstat`
 *Note:* Users can appeal gbans or report spammers at {SUPPORT_CHAT}
 
-Saitama also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
+TakeshiBot also integrates @Spamwatch API into gbans to remove Spammers as much as possible from your chatroom!
 *What is SpamWatch?*
 SpamWatch maintains a large constantly updated ban-list of spambots, trolls, bitcoin spammers and unsavoury characters[.](https://telegra.ph/file/f584b643c6f4be0b1de53.jpg)
 Constantly help banning spammers off from your group automatically So, you wont have to worry about spammers storming your group.
